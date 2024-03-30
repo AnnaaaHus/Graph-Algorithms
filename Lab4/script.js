00314ad55b05e7a6be21e254dc0d80fc03f649f5
AnnaaaHus <annaaahus@gmail.com>
2024-03-30 14:00:55 +0200

    add check for input values

--- Lab4/script.js
+++ Lab4/script.js
@@ -1,4 +1,4 @@
-// Define a graph using an adjacency list
+
 const graph = {
     1: { 2: 10, 3: 3, 6: 1 },      
     2: { 1: 10, 4: 8, 6: 11 },  
@@ -9,52 +9,45 @@ const graph = {
 };
 
 function dijkstra(graph, start) {
-    // Create an object to store the shortest distance from the start node to every other node
-    let distances = {};
 
-    // A set to keep track of all visited nodes
+    let distances = {};
     let visited = new Set();
 
-    // Get all the nodes of the graph
+
     let nodes = Object.keys(graph);
 
-    // Initially, set the shortest distance to every node as Infinity
     for (let node of nodes) {
         distances[node] = Infinity;
     }
-    
-    // The distance from the start node to itself is 0
+
     distances[start] = 0;
 
-    // Loop until all nodes are visited
     while (nodes.length) {
-        // Sort nodes by distance and pick the closest unvisited node
+
         nodes.sort((a, b) => distances[a] - distances[b]);
         let closestNode = nodes.shift();
 
-        // If the shortest distance to the closest node is still Infinity, then remaining nodes are unreachable and we can break
+
         if (distances[closestNode] === Infinity) break;
 
-        // Mark the chosen node as visited
+
         visited.add(closestNode);
 
-        // For each neighboring node of the current node
+
         for (let neighbor in graph[closestNode]) {
-            // If the neighbor hasn't been visited yet
+
             if (!visited.has(neighbor)) {
-                // Calculate tentative distance to the neighboring node
+
                 let newDistance = distances[closestNode] + graph[closestNode][neighbor];
-                
-                // If the newly calculated distance is shorter than the previously known distance to this neighbor
+
                 if (newDistance < distances[neighbor]) {
-                    // Update the shortest distance to this neighbor
+
                     distances[neighbor] = newDistance;
                 }
             }
         }
     }
 
-    // Return the shortest distance from the start node to all nodes
     return distances;
 }
 
@@ -67,6 +60,10 @@ let userResult = document.getElementById("result");
   let startVertex = document.getElementById("vertex").value;
   
 
+if (isNaN(startVertex) || startVertex < 0 || startVertex > 6) {
+    alert("Please enter a valid vertex (0-6)");
+    return;
+  }
   if(startVertex < 0 || startVertex > 6){
     alert("Please enter a valid vertex");
     return;
@@ -75,7 +72,9 @@ let userResult = document.getElementById("result");
     alert("Please enter a vertex");
     return;
   }
+
   console.log(dijkstra(graph, startVertex));
   let result = dijkstra(graph, startVertex);
-document.getElementById("result").innerHTML = 'RESULT : '+ JSON.stringify(result);
+  document.getElementById("result").innerHTML = 'RESULT : '+ JSON.stringify(result);
+
 }
